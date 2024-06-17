49d3257aae92bafd6bd76134e33381b58d525b88
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2024-06-17 09:14:33 +0300

    feat(parse): parse request segments
    parse request segments & handle /echo endpoint, which responds back with
    whatever segment that comes after.

--- app/main.py
+++ app/main.py
@@ -10,14 +10,17 @@ def main():
             data = conn.recv(1024).split(b"\r\n")
             requestInfo = data[0].split(b" ")
             requestMethod = requestInfo[0].decode()
-            requestPath = requestInfo[1].decode()
+            requestSegments = requestInfo[1].decode().split("/")
 
             print("Method:", requestMethod)
-            print("Path:", requestPath)
+            print("Segments:", requestSegments)
 
-            match requestPath:
-                case "/":
+            match requestSegments[1]:
+                case "":
                     conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+                case "echo":
+                    response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(requestSegments[2])}\r\n\r\n{requestSegments[2]}"
+                    conn.sendall(response.encode())
                 case _:
                     conn.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
 
